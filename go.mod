70187a8db79ac462b9fe029d41157dfa3b7aa233
mark <leis17@163.com>
2023-01-19 14:55:09 +0800

    add client、informers......

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/onsi/ginkgo/v2 v2.1.4
 	github.com/onsi/gomega v1.19.0
+	k8s.io/api v0.25.0
 	k8s.io/apimachinery v0.25.0
 	k8s.io/client-go v0.25.0
 	k8s.io/code-generator v0.25.0
@@ -25,6 +26,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.8.0 // indirect
+	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
@@ -72,7 +74,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.25.0 // indirect
 	k8s.io/apiextensions-apiserver v0.25.0 // indirect
 	k8s.io/component-base v0.25.0 // indirect
 	k8s.io/gengo v0.0.0-20211129171323-c02415ce4185 // indirect
--- pkg/controllers/fastdeploy/fastdeploy_controller.go
+++ pkg/controllers/fastdeploy/fastdeploy_controller.go
@@ -17,7 +17,15 @@ limitations under the License.
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"embed"
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"text/template"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -25,6 +33,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	appv1 "github.com/mark8s/fastdeploy/pkg/apis/fastdeploy/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 // FastDeployReconciler reconciles a FastDeploy object
@@ -33,9 +42,14 @@ type FastDeployReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+//go:embed template
+var f embed.FS
+
 //+kubebuilder:rbac:groups=app.mark8s.io,resources=fastdeploys,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=app.mark8s.io,resources=fastdeploys/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=app.mark8s.io,resources=fastdeploys/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,16 +61,153 @@ type FastDeployReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.1/pkg/reconcile
 func (r *FastDeployReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log.Log.Info("Reconcile: " + req.NamespacedName.String())
 	_ = log.FromContext(ctx)
+	build := &appv1.FastDeploy{}
+	err := r.Get(ctx, req.NamespacedName, build)
+	if err != nil {
+		log.Log.Error(err, "Get FastDeploy error")
+		return ctrl.Result{}, err
+	}
 
-	// TODO(user): your logic here
+	// 查找缓存中 同key 的 deployment 和 service
+	deploy := &v1.Deployment{}
+	deployment := r.buildDeployment(build)
+	// 建立关联后，删除fastdeploy资源时就会将deploy删除掉
+	err = controllerutil.SetOwnerReference(build, deployment, r.Scheme)
+	if err != nil {
+		log.Log.Error(err, "SetOwnerReference error")
+		return ctrl.Result{}, err
+	}
+
+	err = r.Get(ctx, types.NamespacedName{Name: build.Spec.Name, Namespace: build.Spec.Namespace}, deploy)
+	if err != nil {
+		// 不存在deploy就得创建
+		if errors.IsNotFound(err) {
+			err = r.Create(ctx, deployment)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			log.Log.Info("Create deployment: " + deployment.Name + " on namespace: " + deployment.Namespace)
+		}
+	} else {
+		if err := r.Update(ctx, deployment); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// 建立关联后，删除fastdeploy资源时就会将service也删除掉
+	svc := &corev1.Service{}
+	service := r.buildService(build)
+	err = controllerutil.SetOwnerReference(build, service, r.Scheme)
+	if err != nil {
+		log.Log.Error(err, "SetOwnerReference error")
+		return ctrl.Result{}, err
+	}
+
+	err = r.Get(ctx, types.NamespacedName{Name: build.Spec.Name, Namespace: build.Spec.Namespace}, svc)
+	if err != nil {
+		if errors.IsNotFound(err) && build.Spec.EnableService {
+			if err = r.Create(ctx, service); err != nil {
+				log.Log.Error(err, "Create service failed")
+				return ctrl.Result{}, err
+			}
+			log.Log.Info("Create service: " + service.Name + " on namespace: " + service.Namespace)
+		}
+	} else {
+		if build.Spec.EnableService {
+			svc.Spec.Ports = service.Spec.Ports
+			if err = r.Update(ctx, svc); err != nil {
+				return ctrl.Result{}, err
+			}
+		} else {
+			if err := r.Delete(ctx, svc); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	// 更新 QuickBuild Status
+	r.updateQBStatus(ctx, build)
 
 	return ctrl.Result{}, nil
+}
+
+func (r *FastDeployReconciler) updateQBStatus(ctx context.Context, build *appv1.FastDeploy) {
+	log.Log.Info("Update FastDeploy Status")
+	deploy := &v1.Deployment{}
+	err := r.Get(ctx, types.NamespacedName{Name: build.Spec.Name, Namespace: build.Spec.Namespace}, deploy)
+	if err != nil {
+		log.Log.Error(err, "Not Found Deploy: "+deploy.Name+" On Namespace: "+deploy.Namespace)
+		return
+	}
+	// 先简单点判断
+	if deploy.Status.ReadyReplicas == deploy.Status.Replicas {
+		build.Status.Status = "AllReady"
+	} else {
+		build.Status.Status = "NotReady"
+	}
+
+	svc := &corev1.Service{}
+	err = r.Get(ctx, types.NamespacedName{Name: build.Spec.Name, Namespace: build.Spec.Namespace}, svc)
+	if err != nil {
+		log.Log.Error(err, "Not Found Svc: "+svc.Name+" On Namespace: "+svc.Namespace)
+		return
+	}
+
+	build.Status.ServiceIP = svc.Spec.ClusterIP
+	err = r.Status().Update(ctx, build)
+	if err != nil {
+		log.Log.Error(err, "Update fastdeploy: "+build.Name+" on namespace: "+build.Namespace+" error")
+		return
+	}
+}
+
+func (r *FastDeployReconciler) buildService(build *appv1.FastDeploy) *corev1.Service {
+	s := &corev1.Service{}
+	err := yaml.Unmarshal(r.parseTemplate("service", build), s)
+	if err != nil {
+		log.Log.Error(err, "Build Service Error")
+		return nil
+	}
+	return s
+}
+
+func (r *FastDeployReconciler) buildDeployment(build *appv1.FastDeploy) *v1.Deployment {
+	d := &v1.Deployment{}
+	err := yaml.Unmarshal(r.parseTemplate("deployment", build), d)
+	if err != nil {
+		log.Log.Error(err, "Build Deploy Error")
+		return nil
+	}
+	return d
+}
+
+func (r *FastDeployReconciler) parseTemplate(templateName string, build *appv1.FastDeploy) []byte {
+	fileBytes, err := f.ReadFile("template/" + templateName + ".yaml")
+	if err != nil {
+		log.Log.Error(err, "")
+		return nil
+	}
+
+	tmpl, err := template.New("tpl").Parse(string(fileBytes))
+	if err != nil {
+		log.Log.Error(err, "")
+		return nil
+	}
+	b := new(bytes.Buffer)
+	err = tmpl.Execute(b, build)
+	if err != nil {
+		return nil
+	}
+	return b.Bytes()
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *FastDeployReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&appv1.FastDeploy{}).
+		Owns(&v1.Deployment{}).
+		Owns(&corev1.Service{}).
 		Complete(r)
 }
